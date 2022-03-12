acdbdd0619015c85773381d2c701bf404c4e19b0
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-03-12 15:20:19 +0530

    Add files via upload

---
+++ build/precache-manifest.339cc4f4b7b99931293097360cd242ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a93ee8e12d0b5eccb44de4e6f914b3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05a9846e998db652143c",
+    "url": "/static/js/2.3be9cbd8.chunk.js"
+  },
+  {
+    "revision": "20fe1f981fb1cff1f250cc81b6309bc2",
+    "url": "/static/js/2.3be9cbd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce187ff59e96c919f801",
+    "url": "/static/js/main.2bfe4d7b.chunk.js"
+  },
+  {
+    "revision": "3acb560f53ac3926abdd",
+    "url": "/static/js/runtime-main.ca4f34ed.js"
+  },
+  {
+    "revision": "7f8aaf70759413139cda6cd2b1577fc1",
+    "url": "/static/media/tweet.7f8aaf70.png"
+  }
+]);
