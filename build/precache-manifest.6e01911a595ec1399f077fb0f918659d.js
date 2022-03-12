84ee3acac20bc4eb82a9175418cc3f2de3eb48ea
NobelRoyP <101279727+NobelRoyP@users.noreply.github.com>
2022-03-12 16:10:54 +0530

    Add files via upload

---
+++ build/precache-manifest.6e01911a595ec1399f077fb0f918659d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9374af197d8da3bceae21e8ef5fa57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05a9846e998db652143c",
+    "url": "/static/js/2.3be9cbd8.chunk.js"
+  },
+  {
+    "revision": "20fe1f981fb1cff1f250cc81b6309bc2",
+    "url": "/static/js/2.3be9cbd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e130209257baac89f617",
+    "url": "/static/js/main.af0a8bbd.chunk.js"
+  },
+  {
+    "revision": "3acb560f53ac3926abdd",
+    "url": "/static/js/runtime-main.ca4f34ed.js"
+  },
+  {
+    "revision": "1872321d2c4d6f592c95570acaecbc69",
+    "url": "/static/media/favicon.1872321d.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59d3481258f747c5060414eacb9cca07.js"
+  "/precache-manifest.6e01911a595ec1399f077fb0f918659d.js"
 );
 
 self.addEventListener('message', (event) => {
